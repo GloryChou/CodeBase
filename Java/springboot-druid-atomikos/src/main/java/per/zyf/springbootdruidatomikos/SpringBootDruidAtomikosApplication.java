5506c730da266d1bef1625e8c004a2ca7c8e5638
sofkyle <fightzyf1992@hotmail.com>
2018-05-03 15:21:24 +0800

    commit pom.xml and some configuration

---
+++ Java/springboot-druid-atomikos/src/main/java/per/zyf/springbootdruidatomikos/SpringBootDruidAtomikosApplication.java
@@ -0,0 +1,10 @@
+package per.zyf.springbootdruidatomikos;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootDruidAtomikosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDruidAtomikosApplication.class);
+    }
+}
