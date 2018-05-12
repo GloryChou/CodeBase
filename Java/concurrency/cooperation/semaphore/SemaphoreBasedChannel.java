b10cee68d80faf5294e0ce95785f184a041906dd
glorychou <fightzyf1992@hotmail.com>
2018-05-12 23:49:00 +0800

    update semaphore

--- Java/concurrency/cooperation/semaphore/SemaphoreBasedChannel.java
+++ Java/concurrency/cooperation/semaphore/SemaphoreBasedChannel.java
@@ -1,9 +1,47 @@
 package cooperation.semaphore;
 
+import cooperation.blockingqueue.Channel;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.Semaphore;
+
 /**
  * @author Kyle
  * @create 2018/5/11 22:24
  */
-public class SemaphoreBasedChannel<P> {
+public class SemaphoreBasedChannel<P> implements Channel<P> {
 
+    private final BlockingDeque<P> queue;
+    private final Semaphore semaphore;
+
+    /**
+     *
+     * @param queue
+     * @param flowLimit traffic limit
+     */
+    public SemaphoreBasedChannel(BlockingDeque<P> queue, int flowLimit) {
+        this(queue, flowLimit, false);
+    }
+
+    public SemaphoreBasedChannel(BlockingDeque<P> queue, int flowLimit, boolean isFair) {
+        this.queue = queue;
+        this.semaphore = new Semaphore(flowLimit, isFair);
+    }
+
+    @Override
+    public void put(P product) throws InterruptedException {
+        // get a signal
+        semaphore.acquire();
+        try {
+            queue.put(product);
+        } finally {
+            // return a signal
+            semaphore.release();
+        }
+    }
+
+    @Override
+    public P take() throws InterruptedException {
+        return queue.take();
+    }
 }
