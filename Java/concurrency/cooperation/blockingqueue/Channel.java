84b799ca9d3434e5a78bd01b3508252614a110ca
glorychou <fightzyf1992@hotmail.com>
2018-05-12 23:42:23 +0800

    update channel interface

--- Java/concurrency/cooperation/blockingqueue/Channel.java
+++ Java/concurrency/cooperation/blockingqueue/Channel.java
@@ -5,4 +5,17 @@ package cooperation.blockingqueue;
  * @create 2018/5/11 22:25
  */
 public interface Channel<P> {
+    /**
+     * store a product
+     * @param product
+     * @throws InterruptedException
+     */
+    void put(P product) throws InterruptedException;
+
+    /**
+     * take a product
+     * @return
+     * @throws InterruptedException
+     */
+    P take() throws InterruptedException;
 }
