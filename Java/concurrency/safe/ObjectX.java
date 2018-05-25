fc513ecb48dfb8e29fd50fb057604526e18169a5
glorychou <fightzyf1992@hotmail.com>
2018-05-25 23:51:23 +0800

    post code of Java Runtime

---
+++ Java/concurrency/safe/ObjectX.java
@@ -0,0 +1,30 @@
+package safe;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Kyle
+ * @create 2018/5/25 23:16
+ */
+public class ObjectX implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static AtomicInteger ID_Generator = new AtomicInteger(0);
+    private Date timeCreated = new Date();
+    private int id;
+
+    public ObjectX() {
+        this.id  = ID_Generator.getAndIncrement();
+    }
+
+    public void greet(String message) {
+        String msg = toString() + ":" + message;
+        Debug.info(msg);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + timeCreated + "] ObejectX [" + id + "]";
+    }
+}
