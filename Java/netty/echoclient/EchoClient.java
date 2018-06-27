53fb3295fcb21f96f7f78361c8350f0d0ed307d4
glorychou <fightzyf1992@hotmail.com>
2018-06-27 23:09:28 +0800

    echo client

---
+++ Java/netty/echoclient/EchoClient.java
@@ -0,0 +1,61 @@
+package echoclient;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author Kyle
+ * @create 2018/6/27 22:54
+ */
+public class EchoClient {
+    private final String host;
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        // 创建Bootstrap
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    // 适用于NIO传输的Channel类型
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    // 向ChannelPipeline中添加一个EchoClientHandler实例
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            
+                        }
+                    });
+            // 连接到远程节点，阻塞直到连接完成
+            ChannelFuture f = b.connect().sync();
+            // 阻塞直到Channel关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 关闭线程池并且释放所有的资源
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: " + EchoClient.class.getSimpleName() + " <host> <port>");
+            return;
+        }
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        new EchoClient(host, port).start();
+    }
+}
