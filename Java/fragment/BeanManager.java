f2b391a3012ff24c7498d0c8c37be850dc797250
glorychou <fightzyf1992@hotmail.com>
2018-08-15 23:59:38 +0800

    post

--- Java/fragment/BeanManager.java
+++ Java/fragment/BeanManager.java
@@ -3,5 +3,12 @@
  * @create 2018/8/15 23:53
  */
 public class BeanManager {
+    public static <T, S> T transformObj(S s) {
+        return null;
+    }
+
+    public static <T, S> List<T> transformList(List<S> s) {
+        return null;
+    }
 
 }
