b17135dbe099d70cbb06bbba6a29d9d202269703
zhouyongfa <fightzyf1992@hotmail.com>
2019-12-11 12:03:35 +0800

    customize non-reentrant lock

---
+++ Java/aqs/src/main/java/per/zyf/aqs/NonReentrantLock.java
@@ -0,0 +1,95 @@
+package per.zyf.aqs;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author: Kyle
+ */
+public class NonReentrantLock implements Lock, Serializable {
+
+    /**
+     * 创建自定义AQS
+     */
+    private final Sync sync = new Sync();
+
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    public boolean tryLock(long timeout, TimeUnit timeUnit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, timeUnit.toNanos(timeout));
+    }
+
+    public void unlock() {
+        sync.release(1);
+    }
+
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    public boolean isLocked() {
+        return sync.isHeldExclusively();
+    }
+
+    /**
+     * 自定义AQS
+     */
+    private static class Sync extends AbstractQueuedSynchronizer {
+        /**
+         * 是否独占了锁
+         * @return
+         */
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        /**
+         * 如果state为0，则尝试获取锁
+         * @param acquires
+         * @return
+         */
+        @Override
+        public boolean tryAcquire(int acquires) {
+            assert acquires == 1;
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * 尝试释放锁，设置state = 0
+         * @param release
+         * @return
+         */
+        @Override
+        protected boolean tryRelease(int release) {
+            assert release == 1;
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(Thread.currentThread());
+            setState(0);
+            return true;
+        }
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+    }
+}
